5e080883257dbb797ad9736edf877668a49665bc
Anurag <anuragmarketing101@gmail.com>
2022-12-24 21:03:39 +0530

    lazy-loaded-images

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,27 +5,20 @@ import { BsSun } from "react-icons/bs"
 import { GiHamburgerMenu } from "react-icons/gi"
 import DropdownMenu from './DropdownMenu'
 
-// lg:bg-[#272727] 
 
 function Navbar() {
   return (
     <div className='bg-[#272727]  fixed top-0  z-50  w-full h-[10vh] px-2 flex flex-row justify-between items-center
-    lg:bg-[#272727] lg:w-[80%] lg:px-10
-     
-    
-    
-    '>
+    lg:bg-[#272727] lg:w-[80%] lg:px-10 '>
 
-        <DropdownMenu />
-        
-        <SearchBar />
-        <GiHamburgerMenu className='text-white hover:cursor-pointer lg:hidden' onClick={() => {
-            const sideBarClass = document.querySelector(".addedSidebarClass")
-            sideBarClass.classList.toggle("mobileSideBar")
-            setTimeout(() => {
-                console.log(sideBarClass);
-            }, 4000);
-        }} />
+      <DropdownMenu />
+
+      <SearchBar />
+      <GiHamburgerMenu className='text-white hover:cursor-pointer lg:hidden' onClick={() => {
+        const sideBarClass = document.querySelector(".addedSidebarClass")
+        sideBarClass.classList.toggle("mobileSideBar")
+
+      }} />
 
     </div>
   )
