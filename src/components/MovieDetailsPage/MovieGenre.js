a7f2b40af585fa16ef5752447cd7be62cf6096be
Anurag <anuragmarketing101@gmail.com>
2022-12-25 00:21:24 +0530

    movie-genre-container-FIXED

--- src/components/MovieDetailsPage/MovieGenre.js
+++ src/components/MovieDetailsPage/MovieGenre.js
@@ -3,23 +3,17 @@ import { Link } from 'react-router-dom'
 import "../../App.css"
 
 
-function MovieGenre( {data} ) {
+function MovieGenre({ data }) {
   return (
 
-    <div className='w-full flex flex-row justify-around items-center py-3 mx-2 overflow-x-scroll  
-     lg:w-[60%] lg:overflow-x-hidden
+    <div className='w-[80%] flex flex-row flex-wrap justify-around items-center px-0 py-2 mx-2 overflow-x-scroll  
+     lg:w-[60%] lg:overflow-x-hidden '>
     
-    '>
-        {data?.genres && data?.genres?.map((genre, index) => {
-            return (
-                
-                  <div key={index} className="w-auto px-5 py-2 mx-1 bg-gray-800 rounded-xl flex flex-row justify-center items-center">
-                      <p className='text-gray-300 font-semibold'> {genre.name} </p>
-                  </div>
-                
-                
-            )
-        })}
+      {data?.genres && data?.genres?.map((genre, index) => {
+        return (
+          <button key={index} className="px-5 py-2 m-2 bg-gray-800 text-gray-300 font-semibold rounded-xl flex flex-row justify-center items-center" > {genre.name} </button>
+        )
+      })}
     </div>
   )
 }
